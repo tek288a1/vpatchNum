6e419b6f22cdd919bf1259b87037f123e821c1b7
tae <tek288a1@gmail.com>
2018-07-22 13:31:26 -0400

    debugging

--- matlab/scripts/calcA_fnc.m
+++ matlab/scripts/calcA_fnc.m
@@ -1,33 +1,34 @@
 function calcA_fnc(n_in, rho_in, beta_opt, K)
-%% evenly spaced-out rho data
-addpath('../functions')
-%n_in = 256;
-%K = 32;
-%beta_opt = 1;
-%rho_in = 0.85;
-indir = '../../data/data.fortran/';
-outdir = '../../data/data.matlab/';
-
-tic
-    infile = sprintf('vp_b%1d_n%02d_r%04d.dat',...
-                     beta_opt, log2(n_in), round(rho_in*1e4));
+% calcA_fnc is a function file calculating A matrix.
+% inputs:  n_in      number of coefficients + velocity in the initial data
+%          rho_in    rho
+%          beta_opt  0, 1, 2
+%          K         dimension of A
+    addpath('../functions')
+    indir = '../../data/data.fortran/';
+    outdir = '../../data/data.matlab/';
+    infile = ...
+        sprintf('vp_b%1d_n%02d_r%04d.dat', ...
+                beta_opt, log2(n_in), round(rho_in*1e4));
+    outfile = ...
+        sprintf('amat_b%1d_n%02d_r%04d.bin',...
+                beta_opt, log2(K), round(rho_in*1e4));
     infilename = strcat(indir, infile);
-    outfile = sprintf('amat_b%1d_n%02d_r%04d.bin',...
-                      beta_opt, log2(K), round(rho_in*1e4));
     outfilename = strcat(outdir, outfile);
-
     raw = load( infilename );
-    %n = raw(1);
+%     n = raw(1);
     rho = raw(2);
     beta = raw(3);
     a = raw(4:end);
     
     fprintf('** Calculating A for rho = %7.4f and K = %5d **\n', rho, K);
-    A = calcA(K, rho, beta, a);
+    tic
+%     A = calcA(K, rho, beta, a);
+    A = rand(K);
+    toc
     
     fid = fopen(outfilename, 'w+');
     fwrite(fid, A, 'double');
     fclose(fid);
-toc
-rmpath('../functions')
+    rmpath('../functions')
 end
